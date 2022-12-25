9d1521f104688b6843a5604fc5b6763f1db61e85
xXkillgamerXx <71557317+xXkillgamerXx@users.noreply.github.com>
2022-12-24 20:13:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-nyc9ys
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-nyc9ys)
---
+++ app/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <Page>
+    <ActionBar title="ActionBar Title">
+      <ActionItem
+        @tap="onTapShare"
+        ios.systemIcon="9"
+        ios.position="left"
+        android.systemIcon="ic_menu_share"
+        android.position="actionBar"
+      />
+      <ActionItem
+        @tap="onTapDelete"
+        ios.systemIcon="16"
+        ios.position="right"
+        text="delete"
+        android.position="popup"
+      />p
+    </ActionBar>
+
+    <GridLayout>
+      <Label
+        class="text-xl align-middle text-center text-gray-500"
+        :text="message"
+        @tap="logMessage"
+      />
+    </GridLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue';
+
+export default Vue.extend({
+  computed: {
+    message() {
+      return 'Hola mundo';
+    },
+  },
+
+  methods: {
+    logMessage() {
+      console.log('You have tapped the message!');
+    },
+    onTapDelete() {
+      console.log('onTapDelete');
+    },
+    onTapShare() {
+      console.log('onTapShare');
+    },
+  },
+});
+</script>
+
+<style>
+/* .info {
+    font-size: 20;
+  } */
+</style>
